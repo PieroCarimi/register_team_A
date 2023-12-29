3f1ac15c01721479eaafadce29e26018071eec33
Gaetano <gaetano.alessandretto@gmail.com>
2023-12-29 12:57:45 +0100

    feat(fix): fixed file,files converted from ts to js

--- models/attendance.models.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createAttendance = ({ date, idRegister, }) => { };
-const deleteAttendance = ({ idAttendance, idRegister, idStudent, }) => { };
-const connectStudentToAttendance = ({ idAttendance, idRegister, idStudent, }) => { };
